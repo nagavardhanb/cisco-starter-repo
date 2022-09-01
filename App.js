dc36f71f2d12bf736e29967fe0f032b368cc7223
nagavardhanb <63955044+nagavardhanb@users.noreply.github.com>
2022-09-01 11:02:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='heading'>Sextant</p>
+        <div className='card'>
+        <p>Network details</p>
+      </div>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
